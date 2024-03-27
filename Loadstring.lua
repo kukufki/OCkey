f6a913327cefac1c7204c782636943e33d9ad75c
vantalta1 <139878419+kukufki@users.noreply.github.com>
2024-03-27 21:39:04 +0200

    TEST

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-"https://gist.githubusercontent.com/kw-roblox/ca85436b1326d24285d91e0666f08b46/raw/951dff953182f35f3585597075b24660585b4934/key.lua"
+"https://raw.githubusercontent.com/kukufki/OCkey/main/Key.lua"
